2b0fe72a8312c9ef7394398465d02770f707be29
1n5an1ty <edd@1n5an1ty.com>
2021-05-22 12:19:03 -0400

    Add EF and start building data models

---
+++ src/WebDemo/Data/CompanyDbContext.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using WebDemo.Data.Models;
+
+namespace WebDemo.Data
+{
+    public class CompanyDbContext : DbContext
+    {
+        public virtual DbSet<Alert> Alerts { get; set; }
+    }
+}
---
+++ src/WebDemo/Data/Models/Alert.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace WebDemo.Data.Models
+{
+    public class Alert
+    {
+        [Column("db_id")]
+        public int Id { get; set; }
+
+        [Column("alert_name")]
+        public string Name { get; set; }
+    }
+}
