c2eb2724399292ce14d5765ecb3067c057923948
1n5an1ty <edd@1n5an1ty.com>
2021-05-24 17:09:07 -0400

    MySql fix

--- src/WebDemo/Data/CompanyDbContext.cs
+++ src/WebDemo/Data/CompanyDbContext.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
 using System.Linq;
 using System.Web;
 using WebDemo.Data.Models;
@@ -15,6 +16,11 @@ namespace WebDemo.Data
 
         public CompanyDbContext() : base("CompanyDbContext")
         { 
+        }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
         }
     }
 }
